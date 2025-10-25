6bd27638cbc8864a0f3369b51a5e8d9fed473457
Lester Arteaga <lesterarte@gmail.com>
2025-10-25 17:04:14 -0600

    Aplicando .gitignore correctamente

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+export default function Home() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <h1
+        style={{
+          fontSize: 100,
+          textAlign: 'center',
+        }}
+      >
+        Acabamos de explicarle que no entre a enlaces dudosos
+      </h1>
+    </div>
+  );
+}
